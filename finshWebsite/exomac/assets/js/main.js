2fd2e2f847d5444cf14763c39eac06f37b902c1b
AbdoAmer <abdoamer067@gmail.com>
2023-11-16 11:46:05 +0200

    16-11-2023

--- finshWebsite/exomac/assets/js/main.js
+++ finshWebsite/exomac/assets/js/main.js
@@ -443,8 +443,123 @@ page.currentYear = d.getFullYear();
 
 function showHide() {
   const toggleNotification = document.querySelector(".notification-box");
-  if (toggleNotification.style.display == "none") { 
-  toggleNotification.style.display = "block";
+  if (toggleNotification.style.display == "none") {
+    toggleNotification.style.display = "block";
+  } else toggleNotification.style.display = "none";
 }
-else toggleNotification.style.display = "none";
+/*=========== pagination ================ */
+function getPageList(totalPages, page, maxLength) {
+  function range(start, end) {
+    return Array.from(Array(end - start + 1), (_, i) => i + start);
+  }
+
+  var slideWidth = maxLength < 9 ? 1 : 2;
+  var leftWidth = (maxLength - slideWidth * 2 - 3) >> 1;
+  var rightWidth = (maxLength - slideWidth * 2 - 3) >> 1;
+
+  if (totalPages <= maxLength) {
+    return range(1, totalPages);
+  }
+
+  if (page <= maxLength - slideWidth - 1 - rightWidth) {
+    return range(1, maxLength - slideWidth - 1).concat(
+      0,
+      range(totalPages - slideWidth + 1, totalPages)
+    );
+  }
+
+  if (page >= totalPages - slideWidth - 1 - rightWidth) {
+    return range(1, slideWidth).concat(
+      0,
+      range(totalPages - slideWidth - 1 - rightWidth - leftWidth, totalPages)
+    );
+  }
+
+  return range(1, slideWidth).concat(
+    0,
+    range(page - leftWidth, page + rightWidth),
+    0,
+    range(totalPages - slideWidth + 1, totalPages)
+  );
 }
+
+$(function () {
+  var numberOfItem = $(".accordion .item").length;
+  var limitPerPage = 5;
+  var totalPages = Math.ceil(numberOfItem / limitPerPage);
+  var paginationSize = 3;
+  var currentPage;
+
+  function showPage(whichPage) {
+    if (whichPage < 1 || whichPage > totalPages) return false;
+
+    currentPage = whichPage;
+
+    $(".accordion .item")
+      .hide()
+      .slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage)
+      .show();
+
+    $(".pagination li").slice(1, -1).remove();
+
+    getPageList(totalPages, currentPage, paginationSize).forEach((item) => {
+      $("<li>")
+        .addClass("page-item")
+        .addClass(item ? "current-page" : "dots")
+        .toggleClass("active", item === currentPage)
+        .append(
+          $("<a>")
+            .addClass("page-link")
+            .attr({ href: "javascript:void(0)" })
+            .text(item || "...")
+        )
+        .insertBefore(".next-page");
+    });
+
+    // $(".Previous-page").toggleClass("disable", page === 1);
+    // $(".next-page").toggleClass("disable", page === totalPages);
+    $(".previous-page").toggleClass("disable", currentPage === 1);
+    $(".next-page").toggleClass("disable", currentPage === totalPages);
+
+    return true;
+  }
+
+  $(".pagination").append(
+    $("<li>")
+      .addClass("page-item")
+      .addClass("previous-page")
+      .append(
+        $("<a>")
+          .addClass("page-link")
+          .attr({ href: "javascript:void(0)" })
+          .text("Prev")
+      ),
+    $("<li>")
+      .addClass("page-item")
+      .addClass("next-page")
+      .append(
+        $("<a>")
+          .addClass("page-link")
+          .attr({ href: "javascript:void(0)" })
+          .text("Next")
+      )
+  );
+
+  $(".accordion").show();
+  showPage(1);
+
+  $(document).on(
+    "click",
+    ".pagination li.current-page:not(.active)",
+    function () {
+      return showPage(+$(this).text());
+    }
+  );
+
+  $(".next-page").on("click", function () {
+    return showPage(currentPage + 1);
+  });
+  $(".previous-page").on("click", function () {
+    return showPage(currentPage - 1);
+  });
+});
